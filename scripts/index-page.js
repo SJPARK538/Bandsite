fac39d1553acebb8871426fbc69da44bcea58275
sjpark538 <phi5388@gmail.com>
2022-03-20 18:01:26 -0400

    comment section html and some js

---
+++ scripts/index-page.js
@@ -0,0 +1,21 @@
+// Comment Array
+let comments = [
+    {
+        name: "Connor Walton",
+        date: "02/17/2021",
+        comment:
+        "This is art. This is inexplicable magic express in the puret way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+    },
+    {
+        name: "Emilie Beach",
+        date: "01/09/2021",
+        comment:
+        "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day"
+    },
+    {
+        name: "Miles Acosta",
+        date: "12/20/2021",
+        comment:
+        "I can't stop listening. Every time I hear one of their songs- the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+    }
+]
